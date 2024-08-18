9048d2043ad0c0ec9574e1d99e33cffa1ac4f866
LidetGetachew <lidet.getachew@walletbirr.com>
2024-08-18 03:39:52 +0300

    second commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{alignItems:'center',textAlign:'center', color:'white',}}>
+    <p>© 2023 City Lovers</p>
+
+    </footer>  )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main style={{backgroundColor:'tomato',color:'white', textAlign:'center'}}>
+    <p style={{fontSize:'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
+  </main>
+
+)
+}
+
+export default MainContent
